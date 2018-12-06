39b44f14efc62be7a8a3ae97bdd746de3e8aa69d
J0x3 <J0x3@protonmail.com>
2018-12-06 14:41:41 -0800

    Update v3.cu

--- v3.cu
+++ v3.cu
@@ -5,141 +5,105 @@
 
 __global__ void setup_kernel(curandState *state) {
     int id = threadIdx.x + blockIdx.x * 64;
-    /* Each thread gets same seed, a different sequence 
-       number, no offset */
+    /* Each thread gets same seed, a different sequence number, no offset */
     curand_init(1234, id, 0, &state[id]);
 }
 
-__global__ void generate_kernel(curandState *state, int n, unsigned int *result) {
+__global__ void pod_racing(curandState *state, unsigned long int n, unsigned int *loss, unsigned int *win) {
     int id = threadIdx.x + blockIdx.x * 64;
-    int count = 0;
+    printf("%d, %d\n", threadIdx.x, blockIdx.x);
     unsigned int x;
+    const unsigned int flips[] = { 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1 };
+
     /* Copy state to local memory for efficiency */
     curandState localState = state[id];
+    
     /* Generate pseudo-random unsigned ints */
     for(int i = 0; i < n; i++) {
         x = curand(&localState);
-	printf("%d ", x);
-        /* Check if low bit set */
-        if(x & 1) {
-            count++;
+        unsigned int subCount = 0;
+        unsigned int flip = x % 2;
+
+        if (flip != flips[subCount]) { // 0 based
+            subCount = 0;
+            loss[id] = 1;
+        }
+        else {
+            subCount++;
+            if (subCount == 16) { // 1 based
+                subCount = 0;
+                win[id] = 1;
+            }
         }
     }
+
     /* Copy state back to global memory */
     state[id] = localState;
-    /* Store results */
-    result[id] += count;
 }
-
-__global__ void generate_uniform_kernel(curandState *state, int n, unsigned int *result) {
-    int id = threadIdx.x + blockIdx.x * 64;
-    unsigned int count = 0;
-    float x;
-    /* Copy state to local memory for efficiency */
-    curandState localState = state[id];
-    /* Generate pseudo-random uniforms */
-    for(int i = 0; i < n; i++) {
-        x = curand_uniform(&localState);
-        /* Check if > .5 */
-        if(x > .5) {
-            count++;
-        }
-    }
-    /* Copy state back to global memory */
-    state[id] = localState;
-    /* Store results */
-    result[id] += count;
-}
-
-__global__ void generate_normal_kernel(curandState *state, int n, unsigned int *result) {
-    int id = threadIdx.x + blockIdx.x * 64;
-    unsigned int count = 0;
-    float2 x;
-    /* Copy state to local memory for efficiency */
-    curandState localState = state[id];
-    /* Generate pseudo-random normals */
-    for(int i = 0; i < n/2; i++) {
-        x = curand_normal2(&localState);
-        /* Check if within one standard deviaton */
-        if((x.x > -1.0) && (x.x < 1.0)) {
-            count++;
-        }
-        if((x.y > -1.0) && (x.y < 1.0)) {
-            count++;
-        }
-    }
-    /* Copy state back to global memory */
-    state[id] = localState;
-    /* Store results */
-    result[id] += count;
-}
-
 
 int main(int argc, char *argv[]) {
-    int i;
-    unsigned int total;
+    int deviceCount = 0;
+    cudaGetDeviceCount(&deviceCount);
+
+    if (deviceCount == 0)
+        printf("No CUDA support device found");
+
+    int devNo = 0;
+    cudaDeviceProp p;
+    cudaGetDeviceProperties(&p, devNo);
+
+    dim3 grid = {p.maxGridSize[0], p.maxGridSize[1], p.maxGridSize[2]};
+    dim3 block = {p.maxThreadsDim[0], p.maxThreadsDim[1], p.maxThreadsDim[2]};
+    /****************************************************************************/
+    int totalWin, totalLoss;
     curandState *devStates;
-    unsigned int *devResults, *hostResults;
-    int sampleCount = 10000;
+    unsigned int *d_loss, *d_win, *h_win, *h_loss;
+    unsigned long int trials = 10000;
+
+   /* https://www.sciencedirect.com/topics/computer-science/execution-configuration */
     
     /* Allocate space for results on host */
-    hostResults = (unsigned int *)calloc(64 * 64, sizeof(int));
+    h_win = (unsigned int *)calloc(grid * block, sizeof(int));
+    h_loss = (unsigned int *)calloc(grid * block, sizeof(int));
 
     /* Allocate space for results on device */
-    cudaMalloc((void **)&devResults, 64 * 64 * sizeof(unsigned int));
-
-    /* Set results to 0 */
-    cudaMemset(devResults, 0, 64 * 64 * sizeof(unsigned int));
-
+    cudaMalloc((void **)&d_loss, grid * block * sizeof(unsigned int));
+    cudaMalloc((void **)&d_win, grid * block * sizeof(unsigned int));
+    
+    // (set seed)
     cudaMalloc((void **)&devStates, 64 * 64 * sizeof(curandState));
-    setup_kernel<<<64, 64>>>(devStates);
-    generate_kernel<<<64, 64>>>(devStates, sampleCount, devResults);
+    setup_kernel<<<grid, block>>>(devStates);
+
+
+    totalWin = 0, totalLoss = 0;
+    /* Set results to 0 */
+    cudaMemset(d_loss, 0, grid * block * sizeof(unsigned int));
+    cudaMemset(d_win, 0, grid * block * sizeof(unsigned int));
+
+    // do the thing
+    pod_racing<<<64, 64>>>(devStates, trials, d_loss, d_win);
     
     /* Copy device memory to host */
-    cudaMemcpy(hostResults, devResults, 64 * 64 * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_loss, d_loss, grid * block * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(h_win, d_win, grid * block * sizeof(unsigned int), cudaMemcpyDeviceToHost);
 
     /* Show result */
-    total = 0;
-    for(i = 0; i < 64 * 64; i++) {
-        total += hostResults[i];
-    }
-    //printf("%10.13f\n", (float)total / (64.0f * 64.0f * sampleCount * 50.0f));
-        
-    /* Set results to 0 */
-    cudaMemset(devResults, 0, 64 * 64 * sizeof(unsigned int));
-
-    /* Generate and use uniform pseudo-random  */
-    generate_uniform_kernel<<<64, 64>>>(devStates, sampleCount, devResults);
-
-    /* Copy device memory to host */
-    cudaMemcpy(hostResults, devResults, 64 * 64 * sizeof(unsigned int), cudaMemcpyDeviceToHost);
-
-    /* Show result */
-    total = 0;
-    for(i = 0; i < 64 * 64; i++) {
-        total += hostResults[i];
-    }
-    //printf("%10.13f\n", (float)total / (64.0f * 64.0f * sampleCount * 50.0f));
     
-    /* Set results to 0 */
-    cudaMemset(devResults, 0, 64 * 64 * sizeof(unsigned int));
-
-    /* Generate and use normal pseudo-random  */
-    generate_normal_kernel<<<64, 64>>>(devStates, sampleCount, devResults);
-
-    /* Copy device memory to host */
-    cudaMemcpy(hostResults, devResults, 64 * 64 * sizeof(unsigned int), cudaMemcpyDeviceToHost);
-
-    /* Show result */
-    total = 0;
-    for(i = 0; i < 64 * 64; i++) {
-        total += hostResults[i];
+    for(int i = 0; i < grid * block; i++) {
+        totalWin += h_win[i];
+        totalLoss += h_loss[i];
     }
-    //printf("%10.13f\n", (float)total / (64.0f * 64.0f * sampleCount * 50.0f));
+
+    printf("Total Win: %d\n", totalWin);
+    printf("Total Loss: %d\n", totalLoss);
 
     /* Cleanup */
     cudaFree(devStates);
-    cudaFree(devResults);
-    free(hostResults);
+    cudaFree(h_win);
+    cudaFree(h_loss);
+    cudaFree(d_win);
+    cudaFree(d_loss);
     return EXIT_SUCCESS;
 }
+
+
